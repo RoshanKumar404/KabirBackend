982940471574dbd55b8d76770bb4b119034a024d
Roshankumar404 <roshankuar951@gmail.com>
2025-04-04 15:45:04 +0530

    restart from Scratch

--- src/middlewares/multer.middleware.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer"
-const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'public/tempStorage')
-    },filename:function(req,file,cb){
-        cb(null,file.originalname)
-    }
-})
