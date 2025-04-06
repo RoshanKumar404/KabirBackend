d032a296109b83348b9756678759f6f0d22513ae
Roshankumar404 <roshankuar951@gmail.com>
2025-04-06 10:08:20 +0530

    added login with email or phone feature

--- SRC/Controllers/logIn.controller.js
+++ SRC/Controllers/logIn.controller.js
@@ -4,24 +4,33 @@ import bcrypt from "bcrypt"
 export const Login=async(req,res)=>{
     // res.send("log in page")
     try {
-        const {email,password,PhoneNumber} =req.body;
+        const {identifier,password} =req.body;
         //validating
-        if(!email || !password){
+        if(!identifier|| !password){
             return res.status(500).send({
                 success:false,
                 message:'Enter Your Credentials'
             })
         }
         //checking user by email AND PHONE NUMBER
-        const user=await User.findOne({email});
+        const user=await User.findOne({
+            $or:[
+                {email:identifier},
+                {PhoneNumber:identifier}
+
+            ]
+        });
+        //const userphone=await User.findOne({PhoneNumber})
         //validating user
-        if(!user){
+        if( !user){
             return res.status(500).send({
                 success:false,
                 message:"user does not found.."
             })
         }
         
+        
+
         //checking password
         const Passwordmatching=await bcrypt.compare(password,user.password);
         //validation pass
--- SRC/Models/user.model.js
+++ SRC/Models/user.model.js
@@ -1,57 +1,58 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcrypt';
-const userSchema=new Schema({
-    username:{
-        type :String,
-        required:[true,"username is must"],
-        unique:[true,'this username already taken'],
-        lowercase:true,
-        trim:true
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is must"],
+        unique: [true, 'this username already taken'],
+        lowercase: true,
+        trim: true
     },
-    name:{
+    name: {
 
-type:String,
-required:true,
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:[true,"email is must"],
-        unique:[true,'this email already exist'],
-        lowercase:true,
-        trim:true
+    email: {
+        type: String,
+        required: [true, "email is must"],
+        unique: [true, 'this email already exist'],
+        lowercase: true,
+        trim: true
+    }, PhoneNumber: {
+        type: String,
+        required: [true, 'Phone number is must'],
+        unique: [true, "This Phone Number already exist"]
     },
-    profilePIcture:{
-        type:String
+
+    profilePIcture: {
+        type: String
 
     },
-    PhoneNumber:{
-        type:Number,
-        required:[true, 'Phone number is must'],
-        unique:[true,"This Phone Number alrewady exist"]
+
+    password: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    refreshTOken:{
-        type:String,
+    refreshTOken: {
+        type: String,
 
     },
 },
-{
-    timestamps:true
-}
+    {
+        timestamps: true
+    }
 );
 //using bcrypt to hash the password
-userSchema.pre("save",async function(next) {
-    if(!this.isModified('password')) return next();
-    this.password=await bcrypt.hash(this.password, 13)
+userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 13)
     next();
 })
 
-userSchema.methods.isPasswordCorrect=async function (password) {
-    return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
 }
 
-export  const User =mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema)
